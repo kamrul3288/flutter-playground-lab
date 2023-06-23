3a83c74ce1178912736f0c2892980bb455050e52
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-06-23 21:23:27 +0600

    implement layout main screen

--- layout-example/lib/layout_main_screen.dart
+++ layout-example/lib/layout_main_screen.dart
@@ -1,3 +1,5 @@
+import 'package:common/widget/component_button.dart';
+import 'package:dep_management/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 
 class LayoutMainScreen extends StatelessWidget {
@@ -10,6 +12,46 @@ class LayoutMainScreen extends StatelessWidget {
         title: const Text("Layouts"),
         centerTitle: true,
       ),
+
+
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            //-------------Row 1-----------------
+            Row(
+              children: [
+                ComponentButton(
+                    flex: 80,
+                    title: "Stack",
+                    icon: FontAwesomeIcons.boxesStacked,
+                    onTap: (){
+                     
+                    }
+                ),
+                ComponentButton(
+                  flex: 100,
+                  title: "Column",
+                  icon: FontAwesomeIcons.tableColumns,
+                  onTap: (){
+
+                  },
+                ),
+
+                ComponentButton(
+                  flex: 80,
+                  title: "Row",
+                  icon: FontAwesomeIcons.tableColumns,
+                  onTap: (){
+
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+
     );
   }
 }
