5b57bbcfbc6208b73d41ce9c0831832bf7b90367
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-07-10 08:07:38 +0600

    rename file

--- layout-example/lib/0_layout_main_screen.dart
+++ layout-example/lib/0_layout_main_screen.dart
@@ -1,9 +1,9 @@
 import 'package:common/widget/component_button.dart';
 import 'package:dep_management/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:layout_example/column_screen.dart';
-import 'package:layout_example/row_screen.dart';
-import 'package:layout_example/stack_screen.dart';
+import 'package:layout_example/2_column_screen.dart';
+import 'package:layout_example/3_row_screen.dart';
+import 'package:layout_example/1_stack_screen.dart';
 
 class LayoutMainScreen extends StatelessWidget {
   const LayoutMainScreen({super.key});
@@ -31,6 +31,44 @@ class LayoutMainScreen extends StatelessWidget {
                     onTap: (){
                       Navigator.of(context).push(
                         MaterialPageRoute(builder: (context)=> const StackScreen())
+                      );
+                    }
+                ),
+                ComponentButton(
+                  flex: 100,
+                  title: "Column",
+                  icon: FontAwesomeIcons.tableColumns,
+                  onTap: (){
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context)=> const ColumnScreen())
+                    );
+                  },
+                ),
+
+                ComponentButton(
+                  flex: 80,
+                  title: "Row",
+                  icon: FontAwesomeIcons.tableColumns,
+                  onTap: (){
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context)=> const RowScreen())
+                    );
+                  },
+                ),
+              ],
+            ),
+
+
+            //-------------Row 2-----------------
+            Row(
+              children: [
+                ComponentButton(
+                    flex: 80,
+                    title: "Stack",
+                    icon: FontAwesomeIcons.boxesStacked,
+                    onTap: (){
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context)=> const StackScreen())
                       );
                     }
                 ),
--- layout-example/lib/2_column_screen.dart
+++ layout-example/lib/2_column_screen.dart
@@ -1,4 +1,3 @@
-import 'package:common/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class ColumnScreen extends StatelessWidget {
