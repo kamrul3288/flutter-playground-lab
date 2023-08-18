de00c3dabd9fc312f43cf9661d4d1552aa9f8264
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-08-18 22:16:34 +0600

    rename file name

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:common/widget/component_button.dart';
 import 'package:flutter/material.dart';
 import 'package:dep_management/font_awesome_flutter.dart';
-import 'package:layout_example/0_layout_main_screen.dart';
+import 'package:layout_example/layout_main_screen.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
