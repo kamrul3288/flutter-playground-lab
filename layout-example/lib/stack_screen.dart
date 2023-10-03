f2b1d37e3ebd265864263cd158b820df58b5b723
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-10-03 07:04:14 +0600

    refactor : wrap with ScaffoldInfoActionAppBar and add description

--- layout-example/lib/stack_screen.dart
+++ layout-example/lib/stack_screen.dart
@@ -12,76 +12,79 @@ class StackScreen extends StatelessWidget {
       widgetDescription: "The stack is a widget in Flutter that contains a list of widgets and positions them on top of the other. In other words, the stack allows developers to overlap multiple widgets into a single screen and renders them from bottom to top.",
 
 
-      body:   SafeArea(
-        child: Stack(
-          alignment: AlignmentDirectional.topStart,
-          fit: StackFit.expand,
-          children: [
+      body:   Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: Stack(
+            alignment: AlignmentDirectional.topStart,
+            fit: StackFit.expand,
+            children: [
 
-            Positioned(
-              width: 100,
-              height: 100,
-              child: Container(color: Colors.red,),
-            ),
+              Positioned(
+                width: 100,
+                height: 100,
+                child: Container(color: Colors.red,),
+              ),
 
-            Positioned(
-              width: 80,
-              height: 80,
-              child: Container(color: Colors.blue,),
-            ),
+              Positioned(
+                width: 80,
+                height: 80,
+                child: Container(color: Colors.blue,),
+              ),
 
-            Positioned(
-              width: 60,
-              height: 60,
-              child: Container(color: Colors.amber,),
-            ),
+              Positioned(
+                width: 60,
+                height: 60,
+                child: Container(color: Colors.amber,),
+              ),
 
-            const Positioned(
-              right: 0,
-              child: Text("End Position Text"),
-            ),
+              const Positioned(
+                right: 0,
+                child: Text("End Position Text"),
+              ),
 
-            const Positioned(
-              bottom: 0,
-              left: 0,
-              child: Text("Bottom Start Position Text"),
-            ),
+              const Positioned(
+                bottom: 0,
+                left: 0,
+                child: Text("Bottom Start Position Text"),
+              ),
 
 
-           Align(
-             alignment: Alignment.center,
-             child: Stack(
-               children: [
-                 Container(
-                   width: 100,
-                   height: 100,
-                   decoration:  BoxDecoration(
-                       shape: BoxShape.circle,
-                       color: Colors.red.shade200
-                   ),
-                 ),
-
-                 Positioned(
-                   bottom: 0,
-                   right: 0,
-                   child: Container(
-                     width: 30,
-                     height: 30,
+             Align(
+               alignment: Alignment.center,
+               child: Stack(
+                 children: [
+                   Container(
+                     width: 100,
+                     height: 100,
                      decoration:  BoxDecoration(
-                       shape: BoxShape.circle,
-                       color: Colors.red.shade100
-                     ),
-                     child: const Center(
-                       child: FaIcon(FontAwesomeIcons.pen,size: 15,),
+                         shape: BoxShape.circle,
+                         color: Colors.red.shade200
                      ),
                    ),
-                 )
-               ],
-             ),
-           )
+
+                   Positioned(
+                     bottom: 0,
+                     right: 0,
+                     child: Container(
+                       width: 30,
+                       height: 30,
+                       decoration:  BoxDecoration(
+                         shape: BoxShape.circle,
+                         color: Colors.red.shade100
+                       ),
+                       child: const Center(
+                         child: FaIcon(FontAwesomeIcons.pen,size: 15,),
+                       ),
+                     ),
+                   )
+                 ],
+               ),
+             )
 
 
-          ],
+            ],
+          ),
         ),
       ),
 
