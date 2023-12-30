3cf8d719ce8e3cebc31a45ceef7728217dab867d
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-12-30 18:24:09 +0600

    implement timer bloc

--- bloc-state-management/lib/timer/bloc_ticker.dart
+++ bloc-state-management/lib/timer/bloc_ticker.dart
@@ -1,8 +1,11 @@
 class BlocTicker{
   const BlocTicker();
 
+  // Method to generate a stream of integers for a countdown.
   Stream<int> tick({required int ticks}) {
-    return Stream.periodic(const Duration(seconds: 1), (x) => ticks - x - 1)
-        .take(ticks);
+    return Stream.periodic(
+        const Duration(seconds: 1),  // Generates events at 1-second intervals.
+        (x)=> ticks - x -1
+    ).take(ticks); // Limits the stream to emit a specified number of ticks.
   }
 }
